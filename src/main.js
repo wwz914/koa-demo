77e0bfe6bdc471b90c31448ae1446f10da79bfc1
wwz914 <3262961369@qq.com>
2023-10-30 00:09:46 +0800

    项目的基本优化

--- src/main.js
+++ src/main.js
@@ -1,13 +1,15 @@
 const Koa=require('koa')
 
+const {APP_PORT}=require('./config/config.default')
+
 // 创建服务
 const app=new Koa()
 
 // 中间件
 app.use((ctx,next)=>{
-    ctx.body='hello world!'
+    ctx.body='hello world'
 })
 
 app.listen(3000,()=>{
-    console.log('sever is running on http://localhost:3000');
+    console.log(`sever is running on http://localhost:${APP_PORT}`);
 })
