4572e68c86bc2f072c3c6bd14c2f3e1b0798e231
wwz914 <3262961369@qq.com>
2023-10-31 23:54:02 +0800

    目录结构优化

--- src/app/index.js
+++ src/app/index.js
@@ -1,8 +1,11 @@
 // 该文件主要处理业务，仅用于创建服务，加载路由...采用分层有利于降低代码耦合度
 const Koa=require('koa')
+const {koaBody}=require('koa-body')
 
 // 创建服务
 const app=new Koa()
+
+app.use(koaBody())
 
 const userRouter=require('../router/user.route')
 
